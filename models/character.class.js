54c963442b316e1ee5714d2811eee9c96cdf8171
MarcoBernert <marcobernert111@gmail.com>
2024-04-17 15:48:17 +0200

    added jumping animation IMAGES_JUMPING

--- models/character.class.js
+++ models/character.class.js
@@ -11,7 +11,18 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-24.png',
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
-    ]
+    ];
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ];
     world;
     speed = 10;
     walkking_sound = new Audio('audio/running.mp3')
@@ -20,6 +31,7 @@ class Character extends MovableObject {
         super();
         this.loadImage(`img/2_character_pepe/2_walk/W-21.png`);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.animate();
         this.applyGravity();
     }
@@ -45,10 +57,15 @@ class Character extends MovableObject {
 
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // this.x += this.speed;
-                // walk animation
-                this.playAnimation(this.IMAGES_WALKING);
+
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    // this.x += this.speed;
+                    // walk animation
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
 
