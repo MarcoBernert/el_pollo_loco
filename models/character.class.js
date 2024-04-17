68f96642e2c96fdb82c8b37bab5950073c0aaa8c
MarcoBernert <marcobernert111@gmail.com>
2024-04-17 14:21:01 +0200

    added level

--- models/character.class.js
+++ models/character.class.js
@@ -13,6 +13,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png',
     ]
     world;
+    speed = 10;
 
     constructor() {
         super();
@@ -23,14 +24,31 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
 
-            if (this.world.keyboard.RIGHT) {
+            if (this.world.keyboard.LEFT && this.x > 100) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+            this.world.camera_x = -this.x + 100;
+        }, 1000 / 60);
+
+
+
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // this.x += this.speed;
+                // walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
 
     }
 
--- models/world.class.js
+++ models/world.class.js
@@ -1,23 +1,10 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ]
-    clouds = [
-        new Cloud(),
-        new Cloud(),
-    ]
-    backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-    ]
+    level = level1;
     ctx;
     canvas;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -27,17 +14,21 @@ class World {
         this.setWorld();
     }
 
-    setWorld(){
+    setWorld() {
         this.character.world = this;
     }
 
     draw() {
         this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        this.addObjectsToMap(this.backgroundObjects);
+        this.ctx.translate(this.camera_x, 0);
+
+        this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
         requestAnimationFrame(this.draw.bind(this));
     }
@@ -48,7 +39,21 @@ class World {
         });
     }
 
+    // Bild spiegelverkehrt zeichnen
     addToMap(mo) {
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
-    }
+        // Überprüfen, ob mo.otherDirection true ist
+        if (mo.otherDirection) {
+          // Kontextzustand speichern, um später wiederherzustellen
+          this.ctx.save();
+          // Bild horizontal spiegeln (vertikal: this.ctx.scale(1, -1))
+          this.ctx.scale(-1, 1);
+          // Bild zeichnen, linksbündig, indem x-Position und Breite negativ gesetzt werden
+          this.ctx.drawImage(mo.img, -mo.x - mo.width, mo.y, mo.width, mo.height);
+          // Gespeicherten Kontextzustand wiederherstellen, um andere Zeichnungen nicht zu beeinflussen
+          this.ctx.restore();
+        } else {
+          // Normales Bild zeichnen, ohne Spiegelung
+          this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        }
+      }
 }
