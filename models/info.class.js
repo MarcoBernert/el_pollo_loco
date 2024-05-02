346d2b2601e3c31df66f2cdf6ea1ccfc4b675e27
MarcoBernert <marcobernert111@gmail.com>
2024-05-02 13:27:14 +0200

    info toggle function

--- models/info.class.js
+++ models/info.class.js
@@ -1,36 +1,3 @@
-// class Info extends MovableObject {
-//     width = 40;
-//     height = 40;
-//     x = 200;
-//     y = 101;
-//     canvas;
-
-//     constructor(canvas) {
-//         super();
-//         this.canvas = canvas;
-//         this.loadImage(`img/10_settings/info-circle-svgrepo-com.svg`);
-        
-//         // Eventlistener für Mausbewegungen hinzufügen
-//         this.canvas.addEventListener('click', (event) => this.handleClick(event));
-
-//     }
-
-//     isMouseOverButton(mouseX, mouseY) {
-//         return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
-//     }
-
-//     handleClick(event) {
-//         var mouseX = event.clientX - this.canvas.getBoundingClientRect().left;
-//         var mouseY = event.clientY - this.canvas.getBoundingClientRect().top;
-
-//         if (this.isMouseOverButton(mouseX, mouseY)) {
-//             let infoSection = document.getElementById('infoSection');
-//             infoSection.classList.remove('d-none');
-//         }
-//     }
-
-// }
-
 class Info extends MovableObject {
     width = 40;
     height = 40;
