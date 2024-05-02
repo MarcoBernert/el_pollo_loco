097ffec27837d3ecec7c1686bf3af024460cba7a
MarcoBernert <marcobernert111@gmail.com>
2024-05-02 11:53:54 +0200

    added info section

--- models/full-screen.class.js
+++ models/full-screen.class.js
@@ -1,3 +1,40 @@
+// class FullScreen extends MovableObject {
+//     width = 30;
+//     height = 30;
+//     y = 60;
+//     x = 205;
+//     canvas;
+//     full = false;
+
+//     constructor(canvas) {
+//         super();
+//         this.canvas = canvas;
+//         this.loadImage(`img/10_settings/fullscreen.svg`);
+//         this.canvas.addEventListener('click', () => this.handleClick()); // Nach Mausklick Funktion ausführen
+//     }
+
+//     isMouseOverButton(mouseX, mouseY) {
+//         return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+//     }
+
+//     handleClick() {
+//         // Vollbildmodus aktivieren, wenn das SVG geklickt wurde
+//         if (this.isMouseOverButton(event.offsetX, event.offsetY)) {
+//             this.canvas.requestFullscreen();
+//             console.log('click fullscreen');
+//             this.fullscreenOn = true;
+//         }
+//     }
+
+//     checkIfFullscreen() {
+//         if (document.fullscreenElement === this.canvas) {
+//             this.fullscreenOn = true;
+//         } else {
+//             this.fullscreenOn = false;
+//         }
+//     }
+// }
+
 class FullScreen extends MovableObject {
     width = 30;
     height = 30;
@@ -5,7 +42,7 @@ class FullScreen extends MovableObject {
     x = 205;
     canvas;
     full = false;
-
+    world;
     constructor(canvas) {
         super();
         this.canvas = canvas;
@@ -20,23 +57,102 @@ class FullScreen extends MovableObject {
     handleClick() {
         // Vollbildmodus aktivieren, wenn das SVG geklickt wurde
         if (this.isMouseOverButton(event.offsetX, event.offsetY)) {
-            this.canvas.requestFullscreen();
+            // let fullscreen = document.getElementById('fullscreenArea');
+            this.enterFullscreen(this.canvas);
             console.log('click fullscreen');
             this.fullscreenOn = true;
         }
     }
 
-    checkIfFullscreen() {
-        if (document.fullscreenElement === this.canvas) {
-            this.fullscreenOn = true;
-        } else {
-            this.fullscreenOn = false;
+    // enterFullscreen(element) {
+    //     if (element.requestFullscreen) {
+    //         element.requestFullscreen();
+    //     } else if (element.msRequestFullscreen) {      // for IE11 (remove June 15, 2022)
+    //         element.msRequestFullscreen();
+    //     } else if (element.webkitRequestFullscreen) {  // iOS Safari
+    //         element.webkitRequestFullscreen();
+    //     }
+    // }
+
+    enterFullscreen() {
+        this.canvas.requestFullscreen();
+    }
+
+
+
+
+    exitFullscreen() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
         }
     }
+
 }
 
 
+// class FullScreen extends MovableObject {
+//     // width = 20;
+//     // height = 20;
+//     // y = 30;
+//     // x = 240;
+//     // canvas;
+
+//     width = 30;
+//     height = 30;
+//     y = 60;
+//     x = 205;
+//     canvas;
+//     world;
+    
+
+//     constructor(canvas) {
+//         super();
+//         this.canvas = canvas;
+//         this.loadImage('img/10_settings/fullscreen.svg');
+//         this.canvas.addEventListener('click', () => this.handleClick());
+//     }
 
 
+//     isMouseOverButton(mouseX, mouseY) {
+//         return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+//     }
+
+//     handleClick() {
+//         if (this.isMouseOverButton(event.offsetX, event.offsetY)) {
+//             let fullscreenArea = document.getElementById('fullscreenArea');
+//             this.canvas.style.height = 100 + 'vh'; // Setze die Höhe auf die Bildschirmhöhe
+//             this.canvas.style.width = 100 + 'vw'; // Setze die Breite auf die Bildschirmbreite
+//         }
+    
+//     }
+// }
 
 
+//  fullscreen(event) {
+//     let fullscreen = document.getElementById('mainScreen');
+//     if(event.type === 'click') {
+//         if(!document.fullscreenElement) {
+//             enterFullscreen(fullscreen);
+//             showMenu(false);
+//         } else {
+//             exitFullscreen();
+//         }
+//     }
+// }
+
+
+// /**
+//  * Requests fullscreen mode for the specified element.
+//  * @param {HTMLElement} element - The element to display in fullscreen mode.
+//  */
+// function enterFullscreen(element) {
+//     if(element.requestFullscreen) {
+//       element.requestFullscreen();
+//     } else if(element.msRequestFullscreen) {
+//       element.msRequestFullscreen();
+//     } else if(element.webkitRequestFullscreen) {
+//       element.webkitRequestFullscreen();
+//     }
+//   }
--- models/info.class.js
+++ models/info.class.js
@@ -1,22 +1,51 @@
+// class Info extends MovableObject {
+//     width = 40;
+//     height = 40;
+//     x = 200;
+//     y = 101;
+//     canvas;
+
+//     constructor(canvas) {
+//         super();
+//         this.canvas = canvas;
+//         this.loadImage(`img/10_settings/info-circle-svgrepo-com.svg`);
+        
+//         // Eventlistener für Mausbewegungen hinzufügen
+//         this.canvas.addEventListener('click', (event) => this.handleClick(event));
+
+//     }
+
+//     isMouseOverButton(mouseX, mouseY) {
+//         return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+//     }
+
+//     handleClick(event) {
+//         var mouseX = event.clientX - this.canvas.getBoundingClientRect().left;
+//         var mouseY = event.clientY - this.canvas.getBoundingClientRect().top;
+
+//         if (this.isMouseOverButton(mouseX, mouseY)) {
+//             let infoSection = document.getElementById('infoSection');
+//             infoSection.classList.remove('d-none');
+//         }
+//     }
+
+// }
+
 class Info extends MovableObject {
     width = 40;
     height = 40;
     x = 200;
     y = 101;
     canvas;
+    infoOn = false; // Zustand für Info-Anzeige
+    infoOnImage = 'img/10_settings/info-on.svg';
+    infoOffImage = 'img/10_settings/info-off.svg';
 
     constructor(canvas) {
         super();
         this.canvas = canvas;
-        this.loadImage(`img/10_settings/info-circle-svgrepo-com.svg`);
-        
-        // Eventlistener für Mausbewegungen hinzufügen
+        this.loadImage(this.infoOffImage);
         this.canvas.addEventListener('click', (event) => this.handleClick(event));
-
-    }
-
-    isMouseOverButton(mouseX, mouseY) {
-        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
     }
 
     handleClick(event) {
@@ -24,9 +53,26 @@ class Info extends MovableObject {
         var mouseY = event.clientY - this.canvas.getBoundingClientRect().top;
 
         if (this.isMouseOverButton(mouseX, mouseY)) {
-            alert('Info wurde geklickt!');
+            this.toggleInfo();
         }
     }
 
+    toggleInfo() {
+        this.infoOn = !this.infoOn;
+        if (this.infoOn) {
+            this.loadImage(this.infoOnImage);
+            let infoSection = document.getElementById('infoSection');
+            infoSection.classList.remove('d-none');
+        } else {
+            this.loadImage(this.infoOffImage);
+            let infoSection = document.getElementById('infoSection');
+            infoSection.classList.add('d-none');
+        }
+    }
+
+    isMouseOverButton(mouseX, mouseY) {
+        return mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height;
+    }
 }
+
 
